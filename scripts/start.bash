5bbe7b7f1892e16234cb24da3306758bfee454d8
Bill Mania <bill@manialabs.us>
2022-02-02 11:33:22 -0800

    Added startup systemd scripts for network setup, shutdown monitor, and starting the application.

--- scripts/start.bash
+++ scripts/start.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source /opt/ros/noetic/setup.bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ubuntu/catkin_ws/src/roboquest_ui/src/RobotConsole/node_modules/opencv-build/opencv/build/lib
-source ~/catkin_ws/devel/setup.bash
+
+source /home/ubuntu/catkin_ws/devel/setup.bash
 roslaunch roboquest_ui server.launch
