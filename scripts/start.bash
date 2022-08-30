ea424da68bd23f3789ea9839a38ea0c58a856995
Bill Mania <bill@manialabs.us>
2022-08-30 14:52:55 -0700

    Updated start.bash to run in the container.

--- scripts/start.bash
+++ scripts/start.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 source /opt/ros/noetic/setup.bash
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/src/catkin_ws/src/roboquest_ui/src/RobotConsole/node_modules/opencv-build/opencv/build/lib
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/src/catkin_ws/src/roboquest_ui/src/RobotConsole/node_modules/opencv-build/opencv/build/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/src/catkin_ws/src/roboquest_ui/node_modules/opencv-build/opencv/build/lib
 
 source /usr/src/catkin_ws/devel/setup.bash
 roslaunch roboquest_ui server.launch
