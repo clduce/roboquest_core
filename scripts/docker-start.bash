942e5762df4151abaa0dd46407b90e9863fed421
Bill Mania <bill@manialabs.us>
2022-08-30 17:17:10 -0700

    Correction to path in docker-start.bash.

--- scripts/docker-start.bash
+++ scripts/docker-start.bash
@@ -1,5 +1,3 @@
 #!/usr/bin/env /bin/bash
 
-docker run -it --name roboquest --privileged --rm --net=host -v
-/var/run/dbus:/var/run/dbus  -v /dev/video0:/dev/video0  roboquest /bin/bash --login
-src/roboquest_core/scripts/start.bash
+docker run -it --name roboquest --privileged --rm --net=host -v /var/run/dbus:/var/run/dbus  -v /dev/video0:/dev/video0  roboquest /bin/bash --login /home/ubuntu/catkin_ws/src/roboquest_core/scripts/start.bash
